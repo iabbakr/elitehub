3ebddfac6168aa62edd66c5bbb215f898fa66741
iabbakr <abubakaribrahim603@gmail.com>
2025-07-04 17:09:07 +0100

    added flex to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,25 +1,28 @@
 export default function Footer() {
     return (
-        <footer className="border">
-            <div className="flex justify:between m-3 gap-5">
+        <footer className="border text-center flex flex-col gap-10">
+            <div className="flex justify-between">
 
-                <div className="">
+                <div className=" border justify-start">
                     <p>twitter</p>
                     <p>facebook</p>
                     <p>whatsapp</p>
                 </div>
-                <div className="">
+                <div className="border">
                     <p>twitter</p>
                     <p>facebook</p>
                     <p>whatsapp</p>
                 </div>
-                <div className="">
+                <div className="border ">
                     <p>twitter</p>
                     <p>facebook</p>
                     <p>whatsapp</p>
                 </div>
             </div>
-            <p>elitehub 2025</p>
+            <div className="">
+                <p>&copy; {new Date().getFullYear()} Elitehub. All rights reserved.</p>
+
+            </div>
         </footer>
     )
 }
